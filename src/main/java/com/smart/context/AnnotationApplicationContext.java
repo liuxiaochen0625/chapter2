c915918fe1e4f78927db6cb157682683bd97e233
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2016-03-25 11:50:59 +0800

    Spring

---
+++ src/main/java/com/smart/context/AnnotationApplicationContext.java
@@ -0,0 +1,16 @@
+package com.smart.context;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.smart.Car;
+
+public class AnnotationApplicationContext {
+
+	 public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(Beans.class);
+		Car car =ctx.getBean("car",Car.class);
+		System.out.println(car.getBrand());
+		System.out.println(car.getMaxSpeed());
+	}
+}
